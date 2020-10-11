b7e2a9ed4567883c2b1de10125a904b2fb422284
Ricardo <ricardojavgf@gmail.com>
2020-10-11 19:55:13 -0400

    Nuevo

---
+++ Vector2.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+class vector2
+{
+public;
+vector2(flout _x, flout _y) :
+x(_x),
+y(_y)
+{}
+void print()
+{
+std::cout << "(X,Y) =>"<<x<<","<<y<< std::end1;
+}
+private;
+flout x;
+flout y;
+};
+
