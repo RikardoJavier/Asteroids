81c865de2dc75a57e2b86dfe6872d307fb9e25f2
Ricardo <ricardojavgf@gmail.com>
2020-10-21 05:04:56 -0400

    intento 2

--- Vector2.hpp
+++ Vector2.hpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 
 class Vector2
 {
@@ -18,21 +17,3 @@ class Vector2
         float x;
         float y;
 };
-=======
-class vector2
-{
-public;
-vector2(flout _x, flout _y) :
-x(_x),
-y(_y)
-{}
-void print()
-{
-std::cout << "(X,Y) =>"<<x<<","<<y<< std::end1;
-}
-private;
-flout x;
-flout y;
-};
-
->>>>>>> 7804ecab37501e3987b8cb4d797864c9a36ff66a
